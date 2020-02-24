58714347d024c78fdd6392cf62434598cc931a43
lfal7838 <lashan.faliq@syscolabs.com>
2020-02-24 13:43:38 +0530

    Update css

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -1,12 +1,18 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'courses',
-  templateUrl: './courses.component.html',
-  styleUrls: ['./courses.component.scss']
+  selector: "courses",
+  templateUrl: "./courses.component.html",
+  styleUrls: ["./courses.component.scss"]
 })
 export class CoursesComponent {
   @Input() courses;
   @Input() title;
 
+  constructor(private router: Router) {}
+
+  onClickFinish() {
+    this.router.navigate(["/selection"]);
+  }
 }
--- src/app/selection-menu/selection-menu.component.ts
+++ src/app/selection-menu/selection-menu.component.ts
@@ -1,10 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
 
 @Component({
-  selector: 'selection-menu',
-  templateUrl: './selection-menu.component.html',
-  styleUrls: ['./selection-menu.component.scss']
+  selector: "selection-menu",
+  templateUrl: "./selection-menu.component.html",
+  styleUrls: ["./selection-menu.component.scss"]
 })
 export class SelectionMenuComponent implements OnInit {
   student;
@@ -12,48 +12,55 @@ export class SelectionMenuComponent implements OnInit {
 
   selectionMapping = [
     {
-      id: '1', message: 'Show me the courses that I have completed',
-      redirectUrl: '/completedCourses'
+      id: "1",
+      message: "Show me the courses that I have completed",
+      redirectUrl: "/completedCourses"
     },
     {
-      id: '2', message: 'Show me the eligible courses that I have to take on this semester',
-      redirectUrl: '/eligibleCourses'
+      id: "2",
+      message:
+        "Show me the eligible courses that I have to take on this semester",
+      redirectUrl: "/eligibleCourses"
     },
     {
-      id: '3', message: 'Show me the prerequisites for my degree',
-      redirectUrl: '/prerequisiteCourses'
+      id: "3",
+      message: "Show me the prerequisites for my degree",
+      redirectUrl: "/prerequisiteCourses"
     },
     {
-      id: '4', message: 'Map out all semesters',
-      redirectUrl: '/semesterMap'
+      id: "4",
+      message: "Map out all semesters",
+      redirectUrl: "/semesterMap"
     },
     {
-      id: '5', message: 'Show me the courses offered in the current semester only',
-      redirectUrl: '/currentSemesterCourses'
+      id: "5",
+      message: "Show me the courses offered in the current semester only",
+      redirectUrl: "/currentSemesterCourses"
     },
     {
-      id: '6', message: 'Register me for the coming semester',
-      redirectUrl: '/registerCourses'
+      id: "6",
+      message: "Register me for the coming semester",
+      redirectUrl: "/registerCourses"
     }
   ];
 
-  constructor(private route: ActivatedRoute, private router: Router) { }
+  constructor(private route: ActivatedRoute, private router: Router) {}
 
   ngOnInit() {
     this.route.queryParamMap.subscribe();
-    this.majorId = this.route.snapshot.queryParamMap.get('majorId');
+    this.majorId = this.route.snapshot.queryParamMap.get("majorId");
     this.student = {
-      id: '1',
-      name: 'Lashan'
+      id: "1",
+      name: "Lashan"
     };
   }
 
   onContinue({ form }) {
-    console.log('form', form.value);
+    console.log("form", form.value);
     const currentSelection = this.selectionMapping.filter(
       selection => selection.id === form.value.selection
     )[0];
-    console.log(currentSelection)
+    console.log(currentSelection);
     if (currentSelection) {
       this.router.navigate([currentSelection.redirectUrl]);
     }
